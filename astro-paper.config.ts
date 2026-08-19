16e26f13179c2d9f6b4d0e4aa0b150158aef10f3
entomimic <208122222+entomimic@users.noreply.github.com>
2026-08-20 00:43:02 +0800

    fixed 1 wrong site

--- astro-paper.config.ts
+++ astro-paper.config.ts
@@ -2,7 +2,7 @@ import { defineAstroPaperConfig } from "./src/types/config";
 
 export default defineAstroPaperConfig({
   site: {
-    url: "https://entomimic.github.io/",
+    url: "https://antp0p.github.io/",
     title: "antp0p's Blog; or, Insectness'",
     description: "antp0p's tech blog, discussing game development & game design, and more.",
     author: "antp0p",
